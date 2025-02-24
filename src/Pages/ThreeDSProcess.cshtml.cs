71c332d19a5aec5c003930ceb5811c390fd1d1c6
Javier Sanchez <jfsanchez@agilisa.com>
2025-02-24 18:26:41 -0400

    Add 3D Secure payment processing page
    This commit introduces a new Razor page (`ThreeDSProcess.cshtml`) and its code-behind file to manage the 3D Secure payment process. The existing `ThreeDSIFrame.cshtml` files have been updated to remove form submission logic, redirecting to the new page instead. The form in `ThreeDSMain.cshtml` has been modified to point to the new action URL, and input fields for card details have been enhanced with `maxlength` attributes for improved validation. The JavaScript SDK for 3D Secure is now included in the new page, along with necessary functions for payment verification.

--- src/Pages/ThreeDSProcess.cshtml.cs
+++ src/Pages/ThreeDSProcess.cshtml.cs
@@ -4,18 +4,18 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace Test_Shop_Razor.Pages
 {
     [IgnoreAntiforgeryToken]
-    public class ThreeDSIFrameModel : PageModel
+    public class ThreeDSIProcessModel : PageModel
     {
         [BindProperty(Name = "amount")]
         public decimal Amount { get; set; }
         [BindProperty(Name = "pan")]
-        public string PAN { get; set; }
+        public required string PAN { get; set; }
         [BindProperty(Name = "month")]
         public int ExpMonth { get; set; }
         [BindProperty(Name = "year")]
         public int ExpYear { get; set; }
         [BindProperty(Name = "successUrl")]
-        public string TargetUrl { get; set; }
+        public required string TargetUrl { get; set; }
 
         public void OnGet()
         {
